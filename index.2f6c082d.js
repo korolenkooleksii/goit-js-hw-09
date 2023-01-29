3152a0d814d46a844e3ba9888ec5b7ae1e738899
korolenkooleksii <111232103+korolenkooleksii@users.noreply.github.com>
2023-01-29 09:48:11 +0000

    Deploying to gh-pages from @ korolenkooleksii/goit-js-hw-09@a57183054a4790cbe09062d7bb13c29fc9b74567 🚀

--- index.2f6c082d.js
+++
@@ -1,2 +0,0 @@
-console.log(),console.log(),console.log();
-//# sourceMappingURL=index.2f6c082d.js.map
--- index.b9490886.js
+++
@@ -1,2 +0,0 @@
-console.log(),console.log(),console.log();
-//# sourceMappingURL=index.b9490886.js.map
